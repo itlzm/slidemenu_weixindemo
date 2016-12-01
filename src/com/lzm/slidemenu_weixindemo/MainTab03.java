91d1091649659403c28babd98b7f9a9e88c3b5fb
itlzm <15221003637@163.com>
2016-12-01 11:05:40 +0800

    first commit

---
+++ src/com/lzm/slidemenu_weixindemo/MainTab03.java
@@ -0,0 +1,18 @@
+package com.lzm.slidemenu_weixindemo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainTab03 extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View newsLayout = inflater.inflate(R.layout.main_tab_03, container, false);
+		return newsLayout;
+	}
+
+}
